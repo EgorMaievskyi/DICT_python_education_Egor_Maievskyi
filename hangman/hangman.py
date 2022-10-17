2574987c7d7d3585a0ef6953de35bb69c6b662f1
Heorhii <h.a.maievskyi@student.khai.edu>
2022-10-17 12:13:58 +0300

    Hangman {4-th} stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,6 +2,8 @@ import random
 print("HANGMAN\n The game will be available soon.")
 words = ["jawa", "python" , "cash" , "opinion"]
 word = random.choice(words)
+so_far = "-" * len(word)
+print(word[:3] + so_far[:-3])
 print("Hangman \n Guess the word")
 a = input(">")
 if a == word:
