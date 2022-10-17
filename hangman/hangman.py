317df3bb30ab90306b76b603d9a6bc834a58e814
Heorhii <h.a.maievskyi@student.khai.edu>
2022-10-17 12:05:05 +0300

    Hangman {2-nd} stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,1 +1,10 @@
+import random
 print("HANGMAN\n The game will be available soon.")
+words = ["jawa", "python"]
+word = random.choice(words)
+print("Hangman \n Guess the word")
+a = input(">")
+if a == word:
+    print("You survived")
+else:
+    print("You lost")
