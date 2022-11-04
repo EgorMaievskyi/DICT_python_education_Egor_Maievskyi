b6152d3fd6c4f26360b9521f888de5a30ecabe4b
Heorhii <h.a.maievskyi@student.khai.edu>
2022-11-04 14:21:10 +0200

    DinnerParty {2-st} stage

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -13,4 +13,8 @@ for key in range(int(number)):
     name_input = input("Enter the name of every friend (including you),each on a new line:\n >")
     friends.setdefault(name_input, 0)
 
+total_amount = int(input("Enter the total amount:\n >"))
+for values in friends:
+            amount = total_amount / number
+            friends[values] = round(amount, 2)
 print(friends)
